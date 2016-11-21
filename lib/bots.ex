9f0729ece49354f7cd8e3f4353c8d24ccb262d2f
Dustin Smith <dustinsmith1024>
2016-11-20 21:02:21 -0600

    making group me bot more reusable

--- lib/bots.ex
+++ lib/bots.ex
@@ -1,19 +1,41 @@
 defmodule GroupMe.Bots do
     # BOTS
     # Create
-    def create() do
-
+    # bot[name] required string
+    # bot[group_id] required string
+    # bot[avatar_url] string
+    # bot[callback_url] string
+    # bot[dm_notification] boolean
+    # Token should the a USER token, not a Bot ID.
+    def create(token, _) when token == nil, do: {:error, "Token is required"}
+    def create(token, options) do
+        if !Map.has_key?(options, :name) || Map.has_key?(options, :group_id) do
+            # throw
+            {:error, "Required params name and/or group_id not found."}
+        else
+            url = "https://api.groupme.com/v3/bots?token=#{token}"
+            GroupMe.Request.post(url, %{bot: options})
+        end
+        # %HTTPoison.Response{body: "{\"meta\":{\"code\":201},\"response\":{\"bot\":{\"name\":\"New Piker\",\"bot_id\":\"ba712580bd938ba074a7dcc032\",\"group_id\":\"26869951\",\"group_name\":\"pick one test\",\"avatar_url\":null,\"callback_url\":null,\"dm_notification\":false}}}",
+        #  headers: [{"Cache-Control", "max-age=0, private, must-revalidate"},
+        #   {"Content-Type", "application/json; charset=utf-8"},
+        #   {"Date", "Sun, 20 Nov 2016 18:33:12 GMT"},
+        #   {"Etag", "\"db78ee084addda2b71c0a4ebeb124fd0\""}, {"Server", "nginx/1.10.2"},
+        #   {"Status", "201 Created"}, {"Strict-Transport-Security", "max-age=31536000"},
+        #   {"X-Runtime", "0.025714"}, {"X-Ua-Compatible", "IE=Edge,chrome=1"},
+        #   {"Content-Length", "212"}, {"Connection", "keep-alive"}], status_code: 201}
     end
 
     # Post a Message
     # We want to send bot_id in
+    def post_message(bot_id, message) when bot_id==nil, do: {:error, "Bot ID cannot be nil."}
     def post_message(bot_id, message) do
         url = "https://api.groupme.com/v3/bots/post"
 
         IO.inspect message
         body = %{
-            "text": message,
-            "bot_id": bot_id
+            text: message,
+            bot_id: bot_id
         }
 
         {status, resp} = GroupMe.Request.post(url, body)
@@ -23,8 +45,20 @@ defmodule GroupMe.Bots do
     end
 
     # Index
-    def list() do
+    def list(token) when token == nil, do: {:error, "Token is required"}
+    def list(token) do
+        url = "https://api.groupme.com/v3/bots?token=#{token}"
+        case GroupMe.Request.get(url) do
+            {:ok, response} -> {:ok, response.body}
+            {:error, reason} -> {:error, reason}
+        end
+    end
 
+    def list!(token) do
+        case list(token) do
+            {:ok, response} -> response
+            {:error, reason} -> throw(reason)
+        end
     end
 
     # Destroy
--- lib/http_helpers.ex
+++ lib/http_helpers.ex
@@ -1,34 +1,37 @@
 defmodule GroupMe.Request do
+    use HTTPoison.Base
 
-    def headers() do
-        %{
-            "Content-Type": "application/json"
-        }
+    def process_response_body(body) do
+        # Kill the 'meta' prop
+        case Poison.decode(body) do
+            {:ok, response} ->
+                transform_response(response)
+            {:error, reason} -> reason
+        end
     end
 
-    def parse(response) do
-        Poison.decode(response.body)
+    defp transform_response(response) do
+        case response["response"] do
+            r when is_list(r) ->
+                Enum.map(r, fn(group) ->
+                    Enum.map(group, fn({k, v}) -> {String.to_atom(k), v} end)
+                end)
+            r when is_map(r) ->
+                IO.puts "its a map!"
+                Enum.map(r, fn({k, v}) -> {String.to_atom(k), v} end)
+            r -> r
+        end
     end
-
-    def get(url) do
-        HTTPoison.start
-        HTTPoison.get!(url, headers(), timeout: 600000, recv_timeout: 60000)
-    end
-
-    def post(url, body) do
-        HTTPoison.start
-        body = Poison.encode!(body)
-        IO.inspect body
-
-        HTTPoison.post(url, body, headers(), timeout: 600000, recv_timeout: 60000)
-    end
-
     def pik_user_auth_token() do
         Application.get_env(:pik, :groupme)[:token]
     end
 
     def bot_id() do
         "8f334ddb3045ac6963614c3c02"
+    end
+
+    def access_token() do
+        "sIvJQp3JTuBLaoAly2PRhQpq7EKsaq8iHwEP5xU1"
     end
 
 end
