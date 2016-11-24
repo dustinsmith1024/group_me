672d69e9c04bf4d14037580e593845f32335daf1
Dustin Smith <dustinsmith1024>
2016-11-24 08:32:17 -0600

    breaking up the socket code a bit

--- lib/socket.ex
+++ lib/socket.ex
@@ -1,5 +1,6 @@
+# Reverse engineered from here.
 defmodule GroupMe.Socket do
-    def open() do
+    def open(access_token, user_id) do
         socket = Socket.Web.connect!("push.groupme.com", path: "/faye")
 
         data = %{
@@ -21,17 +22,25 @@ defmodule GroupMe.Socket do
             IO.inspect d
             hd(d)
         {:ping, _ } ->
+            # TODO Do we need a ping here? Or will that not happen until later
             IO.puts "ping!"
             Socket.Web.send!(socket, {:pong, ""})
         end
 
-        IO.puts res["clientId"]
+        subscribe(socket, res["clientId"], access_token, user_id)
+        listen(socket)
+
+    end
+
+    # "sIvJQp3JTuBLaoAly2PRhQpq7EKsaq8iHwEP5xU1"
+    #  <> "44040609"
+    def subscribe(socket, client_id, access_token, user_id) do
         data = %{
             channel: "/meta/subscribe",
-            clientId: res["clientId"],
-            subscription: "/user/" <> "44040609",
+            clientId: client_id,
+            subscription: "/user/#{user_id}",
             ext: %{
-                access_token: "sIvJQp3JTuBLaoAly2PRhQpq7EKsaq8iHwEP5xU1",
+                access_token: access_token,
                 timestamp: DateTime.utc_now
             }
         };
@@ -51,11 +60,7 @@ defmodule GroupMe.Socket do
             IO.puts "ping!"
             Socket.Web.send!(socket, {:pong, ""})
         end
-
-        listen(socket)
-
     end
-
     # TODO: Extract this out somehow?
     # How do listen to multiple users?
     # Move this to a GenSomething and then spawn one per user.
